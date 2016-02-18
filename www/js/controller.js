c1de618690c411f0f70aae9d7ed980fe479fb1d7
marcus gray <abundanceisfree@gmail.com>
2016-02-18 13:44:03 -0800

    edit button added

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,106 +1,23 @@
-angular.module('starter.controllers', [])
+var app = angular.module('starter.controllers', ['ionic']);
 
-.controller('LoginCtrl', function($state, $rootScope, LoginService){
-  var login = this;
+app.controller('ItemCtrl', ['$scope', 'ItemsFactory', 'ItemFactory', '$location',
+    function ($scope, ItemsFactory, ItemFactory, $location) {
 
-  function signin() {
-    LoginService.signin(login.email, login.password, login.appName)
-        .then(function() {
-          $rootScope.$broadcast('authorized');
-          $state.go('tab.home');
-        }, function(error) {
-          console.log(error)
-        })
-  }
+        // callback for ng-click 'editItem':
+        $scope.editItem = function (itemId) {
+            $location.path('/item-detail/' + itemId);
+        };
 
-  function signout(){
-    LoginService.signout()
-        .then(function() {
-          $state.go('tab.login');
-        })
+        // callback for ng-click 'deleteitem':
+        $scope.deleteItem = function (itemId) {
+            ItemFactory.delete({ id: itemId });
+            $scope.items = ItemFactory.query();
+        };
 
-  }
+        // callback for ng-click 'createItem':
+        $scope.createNewItem = function () {
+            $location.path('/item-creation');
+        };
 
-  login.signin = signin;
-  login.signout = signout;
-})
-
-.controller('homeCtrl', function(ItemsModel, $rootScope){
-  var vm = this;
-
-  function getAll() {
-    ItemsModel.all()
-        .then(function (result) {
-          vm.data = result.data.data;
-        });
-  }
-
-  function create(day) {
-    ItemsModel.create(day)
-        .then(function (result) {
-          cancelCreate();
-          getAll();
-        });
-  }
-
-  function update(day) {
-    ItemsModel.update(day.id, day)
-        .then(function (result) {
-          cancelEditing();
-          getAll();
-        });
-  }
-
-  function deleteday(id) {
-    ItemsModel.delete(id)
-        .then(function (result) {
-          cancelEditing();
-          getAll();
-        });
-  }
-
-  function initCreateForm() {
-    vm.newday = { name: '', description: '' };
-  }
-
-  function setEdited(day) {
-    vm.edited = angular.copy(day);
-    vm.isEditing = true;
-  }
-
-  function isCurrent(id) {
-    return vm.edited !== null && vm.edited.id === id;
-  }
-
-  function cancelEditing() {
-    vm.edited = null;
-    vm.isEditing = false;
-  }
-
-  function cancelCreate() {
-    initCreateForm();
-    vm.isCreating = false;
-  }
-
-  vm.days = [];
-  vm.edited = null;
-  vm.isEditing = false;
-  vm.isCreating = false;
-  vm.getAll = getAll;
-  vm.create = create;
-  vm.update = update;
-  vm.delete = deleteDay;
-  vm.setEdited = setEdited;
-  vm.isCurrent = isCurrent;
-  vm.cancelEditing = cancelEditing;
-  vm.cancelCreate = cancelCreate;
-
-  $rootScope.$on('authorized', function() {
-    getAll();
-  });
-
-  initCreateForm();
-      getAll();
-
-});
-
+        $scope.items = ItemsFactory.query();
+    }]);
